3b99757645f5281bfdbda3598fc204cb33d083ae
abhiTronix <abhi.una12@gmail.com>
2024-06-24 00:04:44 +0530

    🐛 FFhelper: Fixed regex expression bugs in `get_supported_demuxers` method.
    - ⚡️ Simplified regex search for demuxers and handled multi-line output correctly.
    - 🎨 Improved output extraction and stripping logic to handle commas within demuxer names.

--- deffcode/ffhelper.py
+++ deffcode/ffhelper.py
@@ -342,16 +342,16 @@ def get_supported_demuxers(path):
 
     **Returns:** List of supported demuxers.
     """
+    # extract and clean FFmpeg output
     demuxers = check_sp_output([path, "-hide_banner", "-demuxers"])
     splitted = [x.decode("utf-8").strip() for x in demuxers.split(b"\n")]
     split_index = [idx for idx, s in enumerate(splitted) if "--" in s][0]
     supported_demuxers = splitted[split_index + 1 : len(splitted) - 1]
-    # compile regex
-    finder = re.compile(r"[a-z0-9_,-]{2,}\s\s")
-    # find all outputs
-    outputs = finder.findall("\n".join(supported_demuxers))
-    # return output findings
-    return [o.strip() for o in outputs]
+    # search all demuxers
+    outputs = [re.search("\s[a-z0-9_,-]{2,}\s", d) for d in supported_demuxers]
+    outputs = [o.group(0) for o in outputs if o]
+    # return demuxers output
+    return [o.strip() if not ("," in o) else o.split(",")[-1].strip() for o in outputs]
 
 
 def extract_device_n_demuxer(path, machine_OS=None, verbose=False):
